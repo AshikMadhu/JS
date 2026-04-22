1304121bd5e1e44c99fece7aac26e05638c31365
AshikMadhu <ashiksjc2025@gmail.com>
2026-04-22 17:43:27 +0530

    Add files via upload

---
+++ Simple_Calc/script.js
@@ -0,0 +1,11 @@
+function add(){
+    let a=Number(document.getElementById("num1").value);
+    let b=Number(document.getElementById("num2").value);
+    document.getElementById("result").value= a+b;
+}
+
+function sub(){
+    let a=Number(document.getElementById("num1").value);
+    let b=Number(document.getElementById("num2").value);
+    document.getElementById("result").value= a-b;
+}
